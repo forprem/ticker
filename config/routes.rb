8dbf1334165b87640995caa0db0cf56d84f81108
Prem <upsei@ups.com>
2018-12-02 05:16:19 +0000

    Registration and Name Details

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
    #root 'welcome#index'
+   devise_for :users, :controllers => { :registrations => "user/registrations" }
    root 'users#my_portfolio'
    get 'my_portfolio', to: 'users#my_portfolio'
    get 'search_stocks', to: 'stocks#search'
+   resources :user_stocks, only: [:create]
+   resources :user_stocks, only: [:create, :destroy]
+   
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
